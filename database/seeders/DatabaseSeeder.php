570914f2897346942da4a4d9acd2cb04bcfa81f2
maikitop <tony.yakzhin07@gmail.com>
2026-06-27 16:52:42 +0300

    done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,13 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'fio' => 'Администратор Системы',
+            'login' => 'admin',
+            'email' => 'admin@test.ru',
+            'phone' => '+7(999)999-99-99',
+            'password' => Hash::make('adm123'),
+            'role' => 'admin'
         ]);
     }
 }
